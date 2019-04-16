fd4d7e7a6ae506152f2bf30cc12ab60e051b7fca
moeC137 <moe.moe@mailbox.org>
2019-04-16 18:24:48 -0400

    Update pw_remover.sh

--- pw_remover.sh
+++ pw_remover.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Anteitung: 
-# 4: Tippe in die Konsole: " chmod +x voss_removerV1.sh " (ohne die Anfuehrungszeichen)
-# 5: Tippe in die Konsole: " sh voss_removerV1.sh " (ohne Anfuehrungszeichen)
+# Usage: 
+# 1. chmod +x this script
+# 2. sh this script
 # Reeeeeeeeeeeeeee it dosent work!!!!11!!einself!
 # Check if "qpdf" and "figlet" are installed on your system, then try again.
 
